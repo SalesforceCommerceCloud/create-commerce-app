02dc335c6232c74d461925f532db073a8736c8a9
Gaurav Khunger <gkhunger@salesforce.com>
2020-02-04 13:44:49 -0500

    Add tsdoc for the CreateCommerceApp command

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { Command, flags } from '@oclif/command'
 
+/**
+ * CreateCommerceApp command allows creating a sample commerce app with name of
+ * the app being the argument passed to the command. It also makes available
+ * a help menu which lists the way to use the command and the options available
+ * with it.
+ * 
+ */
 class CreateCommerceApp extends Command {
   static description = 'Creates a new Headless Commerce Application'
 
@@ -16,6 +23,11 @@ class CreateCommerceApp extends Command {
     }
   ]
 
+  /**
+   * Parses the arguments passed to the command and creates an app in the 
+   * current directory with the argument passed for appName.
+   * 
+   */
   async run() {
     const { args } = this.parse(CreateCommerceApp)
     const appName = args.appName;
