900feb2e10c0f6408b635aa441fb1fc519a0c353
Gaurav Khunger <gkhunger@salesforce.com>
2020-02-12 09:43:12 -0500

    Change the sfcc url back to git

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const SAMPLE_APPS_GIT_REPO_URL = "https://github.com/SalesforceCommerceCloud/sfcc-sample-apps.git";
+export const SAMPLE_APPS_GIT_REPO_URL = "git@github.com:SalesforceCommerceCloud/sfcc-sample-apps.git";
 export const MESSAGE_PROJECT_CREATED = "Created %s in %s. Checkout the `scripts` section of your `package.json` to get started.";
